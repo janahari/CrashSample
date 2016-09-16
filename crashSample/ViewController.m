ef3782a85164f35b903de9cf3c2107d631e9d327
hariprasad jana <jana_hariprasad@yahoo.co.in>
2016-09-16 16:15:07 +0530

    ok first commit

--- crashSample/ViewController.m
+++ crashSample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Crashlytics/Crashlytics.h>
 
 @interface ViewController ()
 
@@ -17,8 +18,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //Programatically create Crash reporter
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = CGRectMake(20, 50, 100, 30);
+    [button setTitle:@"Crash" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(crashButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    
+    
+    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button2.frame = CGRectMake(100, 100, 100, 30);
+    [button2 setTitle:@"Crash2" forState:UIControlStateNormal];
+    [button2 addTarget:self action:@selector(crashButtonTapped2:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button2];
+    
+
+    
+    
+}
+-(IBAction)crashButtonTapped2:(id)sender
+{
+     [[Crashlytics sharedInstance]crash];
+
 }
 
+
+-(IBAction)crashButtonTapped:(id)sender
+{
+    //Send crash to crashlytics
+   // [[Crashlytics sharedInstance]crash];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
